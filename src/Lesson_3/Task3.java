74e221d59ce79ff73d70c66b8aff18aecb03f8c3
Artur <81081941+ciganchik@users.noreply.github.com>
2023-09-20 21:00:34 +0200

    Задание выполнил

--- src/Lesson_3/Task3.java
+++ src/Lesson_3/Task3.java
@@ -3,14 +3,85 @@ package Lesson_3;
 import java.util.Scanner;
 
 public class Task3 {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-//        System.out.print("Введите число: ");
-//        int a = scanner.nextInt();
-//        System.out.printf("Ваше число: %d\n", a);
+        boolean f = true;
+        while (f) {
+            System.out.println();
+            System.out.println("1 - Game 1");
+            System.out.println("2 - Game 2");
+            System.out.println("3 - Game 3");
+            System.out.println("0 - Exit");
+            System.out.print("Выберите игру: ");
 
-        System.out.print("Введите строку: ");
-        String str1 = scanner.nextLine();
-        System.out.printf("Ваша строка \"%s\"\n", str1);
+            if (scanner.hasNextInt()) {
+                int n = scanner.nextInt();
+
+                switch (n) {
+                    case 1:
+                        game1();
+                        break;
+                    case 2:
+                        game2();
+                        break;
+                    case 3:
+                        game3();
+                        break;
+                    case 0:
+                        f = false;
+                        break;
+                    default:
+                        System.out.println("Нет такой игры\nПопробуйте снова");
+                }
+            } else {
+                System.out.println("Вы ввели некорректное число\nПопробуйте снова ");
+                scanner.nextLine();
+            }
+
+        }
+    }
+
+    static boolean isEvan(int n){
+        return n % 2 == 0;
+    }
+    static void game1(){
+        System.out.println("Game #1");
+        System.out.println("Подсчет суммы нечетных положительных чисел");
+        System.out.println("===========================================");
+        System.out.println("Введите целые числа (0 - выход): ");
+
+        int sum = 0;
+        int counter = 0;
+        boolean isNumber = false;
+        int num = 0;
+
+        do{
+            counter++;
+            System.out.print("Введите число: ");
+            isNumber = scanner.hasNextInt();
+            if (isNumber){
+                num = scanner.nextInt();
+                scanner.nextLine();
+                if(num > 0 && !isEvan(num)){
+                    sum += num;
+                }
+            }else {
+                System.out.println("Вы ввели некорректное число\nПопробуйте снова ");
+                scanner.nextLine();
+            }
+
+        }
+        while (!(isNumber && num == 0) );
+
+        System.out.printf("Сумма нечетных положительных чисел: %d;\nКол-во попыток ввода:  %d\n", sum, counter);
+        System.out.println("Завершение работы... ");
+    }
+
+    static void game2(){
+        System.out.println("Game #2");
+    }
+
+    static void game3(){
+        System.out.println("Game #3");
     }
 }
